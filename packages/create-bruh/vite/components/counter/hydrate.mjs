8d70226172ec2451cf5b72ad444abe8bc420a30c
Daniel Ethridge <danielethridge@icloud.com>
2021-10-23 21:57:05 -0400

    Update templates for new api

--- packages/create-bruh/vite/components/counter/hydrate.mjs
+++ packages/create-bruh/vite/components/counter/hydrate.mjs
@@ -1,10 +1,9 @@
-import { MetaElement, t } from "bruh/dom"
+import { hydrateTextNodes, t } from "bruh/dom"
 import { r } from "bruh/reactive"
 
-const counter = document.querySelector(".counter")
-const placeholder = MetaElement.from(counter.querySelector("bruh-textnode"))
-
+const { counterNumber } = hydrateTextNodes()
 const count = r(0)
-placeholder.replaceWith(t(count))
+counterNumber.replaceWith(t(count))
 
-counter.addEventListener("click", () => count.value++)
+document.querySelector(".counter")
+  .addEventListener("click", () => count.value++)
