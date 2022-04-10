e99f10916b76365285a3d1b61e1e701b1f1eecf3
Daniel Ethridge <danielethridge@icloud.com>
2022-04-10 14:16:59 -0400

    Move mod.mjs

---
+++ packages/bruh/mod.mjs
@@ -0,0 +1,3 @@
+export * as dom      from "./src/dom/index.server.mjs"
+export * as reactive from "./src/reactive/index.mjs"
+export * as util     from "./src/util/index.mjs"
--- packages/bruh/src/mod.mjs
+++
@@ -1,3 +0,0 @@
-export * as dom          from "./dom/index.server.mjs"
-export * as reactive     from "./reactive/index.mjs"
-export * as util         from "./util/index.mjs"
